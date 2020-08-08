aef70b8bdef08b87e15becf3ca7c66ae3d63da53
HongKyungWon <hkw7984@naver.com>
2020-08-08 14:51:58 +0900

    EventHanling

--- src/index.js
+++ src/index.js
@@ -4,36 +4,28 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-class ApiExample extends React.Component {
+// Event Handlng * 
+
+class EventHandling extends React.Component {
     constructor(props) {
-        super(props); 
+        super(props);
         this.state = {
-            data: ''
+            isToggleOn : true
         }
     }
-    callApi = () => { // 서버에 접근해 데이터를 가져옴.
-        fetch("http://jsonplaceholder.typicode.com/todos/1")
-        .then(res => res.json())
-        .then(json =>{
-            this.setState({
-                data: json.title
-        });
-    });
+    handleClick= () => {
+        this.setState(state => ({
+            isToggleOn: !this.state.isToggleOn
+        }))
     }
-    componentDidMount() {  // 실제로 받아온 API 함수를 출력
-        this.callApi();
-    }
-    render() { 
-        return( //데이터가 비었을 경우, 있을 경우 2가지를 고려해야 한다.
-            <h3>
-                {this.state.data? this.state.data: '데이터를 불러오는 중입니다.'}
-            </h3>
-        )
+    render() {
+        return (
+            <button onClick = {this.handleClick}>
+                {this.state.isToggleOn? 'ON' : 'OFF'}
+                </button>
+        );
     }
 }
 
-ReactDOM.render(<ApiExample/>, document.getElementById('root'));
-
-// fetch 함수를 통해 서버에서 데이터를 불러오는 예제
-// test
-
+ReactDOM.render(<EventHandling/>,
+     document.getElementById('root'));
